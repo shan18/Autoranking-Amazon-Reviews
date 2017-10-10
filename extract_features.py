5882c316bf6e5dea817d21f6977d93c9a3128fa5
codegeek96 <thegeek.004@gmail.com>
2017-10-11 02:40:33 +0530

    Extract textual features from the dataset

---
+++ extract_features.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from nltk.tokenize import sent_tokenize
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+# Textual Features
+def extract_textual_features(data):
+    # m: total number of training examples
+    m = len(data['review'].values)
+
+    # total number of characters
+    text_length = np.array(data['review'].str.len()).reshape((m, 1))
+
+    # total number of alphabetical characters
+    character_count = np.array(
+        data['review'].replace(regex=True, to_replace=r'[^a-zA-Z]', value=r'').str.len()
+    ).reshape((m, 1))
+
+    # Tokenized Sparse Matrix
+    vectorizer = CountVectorizer(lowercase=True)
+    matrix = vectorizer.fit_transform(np.array(data['review'].values))
+    matrix = np.array(matrix.todense())
+
+    # the number of words in the review text
+    word_count = np.sum(matrix, axis=1, keepdims=True)
+
+    # the number of unique words in the review text
+    unique_word_count = np.count_nonzero(matrix, axis=1).reshape((m, 1))
+
+    # the number of sentences in the review text
+    sentence_count = []
+    for i in np.array(data['review'].values):
+        s = sent_tokenize(i)
+        sentence_count.append(len(s))
+    sentence_count = np.array(sentence_count).reshape((m, 1))
+
+    # Automated Readability Index
+    ARI = 4.71 * (character_count / word_count) + 0.5 * (word_count / sentence_count)
+    ARI = np.reshape(ARI, (m, 1))
+
+    return np.concatenate(
+        (text_length, character_count, word_count, unique_word_count, sentence_count, ARI),
+        axis=1
+    )
--- script.py
+++ script.py
@@ -1,8 +1,13 @@
+import nltk
 import numpy as np
 import matplotlib.pyplot as plt
 
 from get_data import parse_input
 from preprocess_filter_data import preprocess_input, filter_input
+from extract_features import extract_textual_features
+
+# path containing the nltk data on the system
+nltk.data.path.append('/home/shan/Packages/nltk_data')
 
 
 INPUT_FILE = 'reviews_Video_Games_5.json.gz'
@@ -21,6 +26,10 @@ print(df.head())
 df_train = df.sample(frac=0.8)
 df_test = df.loc[~df.index.isin(df_train.index)]
 
+# Preview training dataset
+print('\n\nTraining dataset:\n')
+print(df_train.head())
+
 # Set the outcome variable (Denotes helpfulness of a review)
 upvotes = np.array(df_train['upvotes'].values)
 downvotes = np.array(df_train['downvotes'].values)
@@ -30,3 +39,9 @@ Y_train = upvotes / (upvotes + downvotes)
 plt.hist(Y_train, ec='black')
 plt.xlabel('Helpfulness Score')
 plt.show()
+
+
+# Constructing feature vector
+
+# Textual Features
+X_train = extract_textual_features(df_train)
